1c2d4a08d6e608682865744ccfb87aa17e236b77
dhxh <catincut@foxmail.com>
2023-03-17 05:38:31 +0000

    update blog

---
+++ style/js/music.js
@@ -0,0 +1,25 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+        {
+        name: '君は僕に似ている',
+        artist: 'Piano',
+        url: '/style/music/君は僕に似ている.mp3',
+        cover: '/style/music/Rainbow.jpg',
+    },
+        {
+        name: '星の扉',
+        artist: 'Piano Cover',
+        url: '/style/music/星の扉.mp3',
+        cover: '/style/music/Rainbow.jpg',
+    },
+    {
+        name: '時代を越える想い',
+        artist: 'Piano',
+        url: '/style/music/時代を越える想い.mp3',
+        cover: '/style/music/Rainbow.jpg',
+    }
+    ]
+});
