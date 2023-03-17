b5f7ceaf63a996adedd778eaf8ca655622a3426a
dhxh <catincut@foxmail.com>
2023-03-17 06:26:28 +0000

    update blog

--- style/js/music.js
+++ style/js/music.js
@@ -4,25 +4,24 @@ const ap = new APlayer({
     autoplay: true,
     loop: 'all',
     order: 'random',
-    preload: 'auto',
     audio: [
         {
         name: '君は僕に似ている',
         artist: 'Piano',
-        url: 'https://cdn.jsdelivr.net/gh/dhxh/dhxh.github.io@main/style/music/君は僕に似ている.mp3',
-        cover: 'https://cdn.jsdelivr.net/gh/dhxh/dhxh.github.io@main/style/music/Rainbow.jpg',
+        url: '/style/music/君は僕に似ている.mp3',
+        cover: '/style/music/Rainbow.jpg',
     },
         {
         name: '星の扉',
         artist: 'Piano Cover',
-        url: 'https://cdn.jsdelivr.net/gh/dhxh/dhxh.github.io@main/style/music/星の扉.mp3',
-        cover: 'https://cdn.jsdelivr.net/gh/dhxh/dhxh.github.io@main/style/music/Rainbow.jpg',
+        url: '/style/music/星の扉.mp3',
+        cover: '/style/music/Rainbow.jpg',
     },
     {
         name: '時代を越える想い',
         artist: 'Piano',
-        url: 'https://cdn.jsdelivr.net/gh/dhxh/dhxh.github.io@main/style/music/時代を越える想い.mp3',
-        cover: 'https://cdn.jsdelivr.net/gh/dhxh/dhxh.github.io@main/style/music/Rainbow.jpg',
+        url: '/style/music/時代を越える想い.mp3',
+        cover: '/style/music/Rainbow.jpg',
     }
     ]
 });
